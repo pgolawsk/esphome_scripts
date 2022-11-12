d090f439620634bc0ff158b6b68badf7ad253234
Paweł Goławski <pawel.golawski@2com.pl>
2022-11-12 19:44:44 +0100

    ESP initiatl setup

---
+++ esp12F_setup.sh
@@ -0,0 +1,41 @@
+#* Setup of ESP sensors
+# Pawelo 20221111 first steps
+# Pawelo 20221112, created based on https://github.com/geerlingguy/pico-w-garage-door-sensor
+
+#######################
+#* Configure new network for sensors at home
+# based on: https://wiki.teltonika-networks.com/view/How_to_set_up_a_guest_WiFi_network_on_RUTX
+
+
+#######################
+#* Install ESPHome on ESP-12F board
+# based on: https://esphome.io/index.html
+
+# using Chrome or EDGE, visit: https://esphome.github.io/esp-web-tools/
+# click "Connect" button to flash ESP with basic firmware
+
+# then connect to wifi SENSORS
+# and assign static ip for ESP board
+
+#######################
+#* Install ESP Home on mac/win
+
+# assuming python3 is installed
+pip3 install esphome
+
+
+#######################
+# Create YAML file
+# based on https://www.youtube.com/watch?v=a3iay-g1AsI
+esphome wizard name.yml
+
+# based on https://github.com/geerlingguy/pico-w-garage-door-sensor
+# 1. create secrets.yaml
+
+# 2. write config file
+
+# 3. compile code
+esphome compile name.yml
+
+
+
