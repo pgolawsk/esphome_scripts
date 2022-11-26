974b65a9a0dfd297bb9f1afdb3ed621ed6665dae
Paweł Goławski <pawel.golawski@2com.pl>
2022-11-26 16:37:18 +0100

    further experiments

--- pt02_rs485_setup.sh
+++ pt02_rs485_setup.sh
@@ -1,6 +1,8 @@
 #* Setup of Air Quality Monitor PT02 via RS485
 # Pawelo 20221117, created based on https://github.com/epsilonrt/mbpoll/
 # Pawelo 20221118, further experiments
+# Pawelo 20221122, further experiments with mbpool -l (pool time) -o (timeout)
+# Pawelo 20221123, further experiments tuya-convert
 
 
 pip3 install modbus_cli
@@ -10,10 +12,12 @@ sudo apt install mbpoll
 
 # discover the device id
 for i in {1..247}; do mbpoll -a $i -b 38400 -u /dev/ttyUSB0; done
+for i in {1..247}; do mbpoll -a $i -b 4800 -l 100 -o 0.1 -1 -u /dev/ttyUSB0; done
 
 # test the onnection
 mbpoll -a 33 -b 38400 -t 3 -r 1 -c 2 /dev/ttyUSB0
 
+mbpoll -0 -m rtu -u -l 100 -o 0.1 /dev/ttyUSB0
 
 #####################
 # on MAcOS, based on https://github.com/SciFiDryer/ModbusMechanic#latest-release
@@ -27,3 +31,16 @@ cd ~/ModBusMechanic
 sudo java -jar ModbusMechanic.jar
 
 #! NOT CONENCTED - probably the sensor communication is broken:(
+
+#*Tuya convert experiment, based on: https://lukasz.oksejuk.pl/2020/10/27/tasmota-wgrywanie-za-pomoca-tuya-convert/ and https://github.com/ct-Open-Source/tuya-convert
+# clone tuya convert repo
+git clone https://github.com/ct-Open-Source/tuya-convert
+cd tuya-convert
+./install_prereq.sh
+
+# backup stock firmware
+rfkill unblock all
+ifconfig wlan0 up
+
+# start flashing
+./start_flash.sh
