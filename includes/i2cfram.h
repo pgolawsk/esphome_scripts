19cf8bd506e05f68c5b68e7647353d92142d3c3c
Paweł Goławski <pawel.golawski@2com.pl>
2023-09-23 19:01:27 +0200

    i2c fram try

---
+++ includes/i2cfram.h
@@ -0,0 +1,82 @@
+/**************************************************************************/
+/*!
+    @file     I2CFram.h
+    @author   AGE
+
+    @section  HISTORY
+
+    v0.1 - fIRST TEST RUN
+
+    iNTERFACE for the MB85RC I2C FRAM from Fujitsu.
+
+    @section LICENSE
+
+    Software License Agreement (BSD License)
+
+*/
+/**************************************************************************/
+
+#include "esphome.h"
+#include <FRAM.h>
+
+static const char *const TAG = "I2C-FRAM";
+
+int DeviceID, rv;
+FRAM fram;
+
+class MyFram : public Component
+{
+public:
+  MyFram(int device_id)
+  {
+    DeviceID = device_id;
+  }
+
+  void setup() override
+  {
+    rv = fram.begin(DeviceID);
+  }
+  void loop() override
+  {
+    // ESP_LOGI("custom", "Running ...");
+  }
+
+  void dump_config()
+  {
+    ESP_LOGI(TAG, "Starting I2C FRAM on device is %x ", DeviceID);
+    if (rv != 0)
+    {
+      ESP_LOGI(TAG, "No memory module found %d ", rv);
+    }
+    else
+    {
+      ESP_LOGI(TAG, "Memory module found");
+      uint16_t Size = fram.getSize();
+      ESP_LOGI(TAG, "Memory size %d kbyte", Size);
+    }
+  }
+};
+
+float readfromfram(uint32_t memaddr)
+{
+  float value;
+  memaddr = memaddr * 4;
+  value = fram.readFloat(memaddr);
+  ESP_LOGI(TAG, "read value %f from memory addres %d", value, memaddr);
+  return (value);
+}
+
+void writetofram(uint32_t memaddr, float value)
+{
+  memaddr = memaddr * 4;
+  fram.writeFloat(memaddr, value);
+  ESP_LOGI(TAG, "write value %f to memory addres %d ", value, memaddr);
+}
+
+void readcounters()
+{
+  id(counter_1) = readfromfram(0x01);
+  id(counter_2) = readfromfram(0x02);
+  id(counter_3) = readfromfram(0x03);
+  id(counter_4) = readfromfram(0x03);
+}
