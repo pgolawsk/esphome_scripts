16d2ba955eea434754aa460c1140bf8b3bb07299
Paweł Goławski <pawel.golawski@2com.pl>
2023-09-24 13:55:58 +0200

    corrected the name for default device esp32-35

--- esp_upgrade.sh
+++ esp_upgrade.sh
@@ -15,4 +15,4 @@ esphome -s devicename esp12f-11 -s updates 30s -s room Entrance -s mqtt_room ent
 esphome -s devicename esp12f-15 -s updates 30s -s room Upstairs -s mqtt_room upstairs run esp12f_THP_P.yaml
 esphome -s devicename esp12f-21 -s updates 30s -s room Unrderfloor -s mqtt_location measures -s mqtt_room underfloor run esp12f_THdb_SDr.yaml
 esphome -s devicename esp12f-25 -s updates 30s -s room AquariumWindow -s mqtt_location measures -s mqtt_room aquarium_window run esp12f_THIddb_STr.yaml
-esphome -s devicename esp32-35 -s updates 1min -s room Garage -s mqtt_location measures -s mqtt_room garage run esp32_THIWdb_SBYr.yaml
+esphome -s devicename esp32-35 -s updates 1min -s room Pump -s mqtt_location measures -s mqtt_room pump run esp32_THIWdb_SBYr.yaml
