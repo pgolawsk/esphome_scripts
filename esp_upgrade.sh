b0c60668be2b27fa62ace521e390f614bbd83eec
Paweł Goławski <pawel.golawski@2com.pl>
2026-02-28 14:36:43 +0100

    deployed esp32-06 to PROD

--- esp_upgrade.sh
+++ esp_upgrade.sh
@@ -15,13 +15,14 @@
 # Pawelo, 20250202, added test of compile on each script in 0_DEV, 1_UAT, 2_PROD
 # Pawelo, 20250203, exclude not ready (.yml) scripts from compilation tests
 # Pawelo, 20250125, moved esp32-39 Attic device to 2_PROD
-# Pawelo, 20250220, added venv creation for python3
+# Pawelo, 20250220, added .venv creation for python3
 # Pawelo, 20250221, added "esphome --version" prior testing cycles
 # Pawelo, 20250615, moved esp12f-10 script to 2_PROD
 # Pawelo, 20250823, added example of downgrade of esphome
 # Pawelo, 20251225, reference to aliases for PROD devices from .dir_aliases file
 # Pawelo, 20251230, use check_esphome_version.sh to verify if there is new ESPHome version
 # Pawelo, 20260221, added esp32-06 Garden/Gateway device to 1_UAT for testing before PROD upgrade
+# Pawelo, 20260228, moved esp32-06 Garden/Gateway device to 2_PROD after successful testing and deployment
 
 #*###########################
 #* Check Python VENV
@@ -29,7 +30,7 @@ python --version
 # Python 3.13.2
 
 # if not VENV is active, activate it
-source venv/bin/activate
+source .venv/bin/activate
 
 pip install --upgrade pip
 pip --version
@@ -100,6 +101,7 @@ pip install --upgrade "ltchiptool>=3.0.1,<4.0"
 #* Upgrade PROD ESP devices via OTA
 #! use aliases instead of below PROD, example esp10, esp05, ...
 esphome -s devicename esp32-05 -s updates 1min -s room Shades -s mqtt_location outside -s mqtt_room shades -s room2 WinterGardenUpp -s mqtt_location2 home -s mqtt_room2 winter_garden_upp run 2_PROD/esp32-05_Shades_WinterGardenUpp.yaml
+esphome -s devicename esp32-06 -s updates 1min -s room Garden -s mqtt_location outside -s mqtt_room garden -s room2 Gateway -s mqtt_location2 outside -s mqtt_room2 gateway run 2_PROD/esp32-06_Garden_Gateway.yaml
 esphome -s devicename esp12f-10 -s updates 30s -s room Office -s mqtt_room office run 2_PROD/esp12f-10_Office.yaml
 esphome -s devicename esp12f-11 -s updates 30s -s room Entrance -s mqtt_room entrance -s room2 Entry -s mqtt_location2 outside -s mqtt_room2 entry run 2_PROD/esp12f-11_Entrance_Entry.yaml
 esphome -s devicename esp32-14 -s updates 30s -s room Salon -s mqtt_room salon run 2_PROD/esp32-14_Salon.yaml
@@ -111,7 +113,7 @@ esphome -s devicename esp32-36 -s updates 30s -s room Garage -s mqtt_location ou
 esphome -s devicename esp32-39 -s updates 1min -s room Attic -s mqtt_location home -s mqtt_room attic run 2_PROD/esp32-39_Attic.yaml
 
 #* Future devices via OTA (with "compile" instead of "run")
-esphome -s devicename esp32-06 -s updates 1min -s room Garden -s mqtt_location outside -s mqtt_room garden -s room2 Gateway -s mqtt_location2 outside -s mqtt_room2 gateway compile 1_UAT/esp32-06_Garden_Gateway.yaml
+# esphome -s devicename esp32-06 -s updates 1min -s room Garden -s mqtt_location outside -s mqtt_room garden -s room2 Gateway -s mqtt_location2 outside -s mqtt_room2 gateway compile 1_UAT/esp32-06_Garden_Gateway.yaml
 
 #* New exploring devices
 esphome -s devicename esp12f-29 -s updates 30s -s room Test -s mqtt_location measures -s mqtt_room test run 0_DEV/esp12f_dev.yaml
